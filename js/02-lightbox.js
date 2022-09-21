6b58341ed86fa8d0dbc26635db34da1822aaad4e
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2022-09-21 20:21:11 +0300

    Fix creating lightbox on click only

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ console.log(galleryItems);
 const gallerySimpleLightbox = {
 
     refs: {
-        gallery: document.querySelector('.gallery'),        
+        gallery: document.querySelector('.gallery'),
     },
     lightboxWrapSelector: '.gallery a',
     items: galleryItems,
@@ -19,31 +19,26 @@ const gallerySimpleLightbox = {
         <img class="gallery__image" src="${preview}" alt="${description}"/>
         </a></li>`;
 
-        return template;        
+        return template;
     },
 
     createMarkup() {
         return this.items.map(this.markupTemplate).join('');
     },
 
-    renderMarkup() {
-        this.refs.gallery.insertAdjacentHTML('beforeend', this.createMarkup());
-        this.makeClickable();
-    },
-
-    makeClickable() {
-        this.refs.gallery.addEventListener('click', this.onClick.bind(gallerySimpleLightbox));
-    },
-
-    onClick(evt) {
-        evt.preventDefault();
-
+    createLightbox() {
         const lightbox = new SimpleLightbox(this.lightboxWrapSelector, {
             captionsData: 'alt',
             captionDelay: 250,
             showCounter: false,
         });
+        
         lightbox.on('closed.simplelightbox', () => { lightbox.refresh() });
+    },
+
+    renderMarkup() {
+        this.refs.gallery.insertAdjacentHTML('beforeend', this.createMarkup());
+        this.createLightbox();
     },
 };
 gallerySimpleLightbox.renderMarkup();
