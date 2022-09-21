8d50dd3a1c1a787a802ca19ca7e7f3c63b780eff
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2022-09-21 19:28:23 +0300

    Add task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const gallery = {
+
+    refs: {
+        gallery: document.querySelector('.gallery'),
+    },
+
+    items: galleryItems,
+
+    markupTemplate({ preview, original, description } = {}) {
+        const template = `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+
+        return template;        
+    },
+
+    createMarkup() {
+        return this.items.map(this.markupTemplate).join('');
+    },
+
+    renderMarkup() {
+        this.refs.gallery.insertAdjacentHTML('beforeend', this.createMarkup());
+        this.makeClickable();
+    },
+
+    makeClickable() {
+        this.refs.gallery.addEventListener('click', this.onClick);
+    },
+
+    onClick(evt) {
+        evt.preventDefault();
+
+        if (evt.target.nodeName !== 'IMG') {
+            return;
+        }
+
+        const instance = basicLightbox.create(`
+        <img src="${evt.target.dataset.source}" width="800" height="600">`, {
+            onShow: () => {
+                document.addEventListener('keyup', closeElement);
+            },
+            onClose: () => {
+                document.removeEventListener('keyup', closeElement);
+            }
+        });
+
+        function closeElement(evt) {
+            if (evt.key === 'Escape') {
+                                
+                instance.close();
+            }
+        };
+
+        instance.show();
+    },
+};
+
+gallery.renderMarkup();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const gallerySimpleLightbox = {
+
+    refs: {
+        gallery: document.querySelector('.gallery'),        
+    },
+    lightboxWrapSelector: '.gallery a',
+    items: galleryItems,
+
+    markupTemplate({ preview, original, description } = {}) {
+        const template = `
+        <li class="gallery__item"><a href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a></li>`;
+
+        return template;        
+    },
+
+    createMarkup() {
+        return this.items.map(this.markupTemplate).join('');
+    },
+
+    renderMarkup() {
+        this.refs.gallery.insertAdjacentHTML('beforeend', this.createMarkup());
+        this.makeClickable();
+    },
+
+    makeClickable() {
+        this.refs.gallery.addEventListener('click', this.onClick.bind(gallerySimpleLightbox));
+    },
+
+    onClick(evt) {
+        evt.preventDefault();
+
+        const lightbox = new SimpleLightbox(this.lightboxWrapSelector, {
+            captionsData: 'alt',
+            captionDelay: 250,
+            showCounter: false,
+        });
+        lightbox.on('closed.simplelightbox', () => { lightbox.refresh() });
+    },
+};
+gallerySimpleLightbox.renderMarkup();
